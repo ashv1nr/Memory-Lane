0b19a61024ad6ca724088a20a66d369272c098c5
Ashvin Ramabadran <62221042+ashv1nr@users.noreply.github.com>
2022-01-29 13:06:51 -0600

    holds attributes for each node

--- Node.java
+++ Node.java
@@ -1,42 +1,44 @@
 
 import static java.lang.System.*;
-import java.awt.*;
-//import java.awt.Graphics.*;
-//import java.awt.Frame.*;
+import java.io.*;
 
-/*import javafx.application.Application;
+//import java.awt.*;
+//import java.awt.Frame.*;
+//import java.awt.Graphics.*;
+
+import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.scene.Scene;
-import javafx.scene.text.*;
-import javafx.scene.Group;
+import javafx.geometry.*;
+import javafx.stage.Stage;
+
+import javafx.scene.control.*;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.Group;
+import javafx.scene.layout.*;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
-import javafx.scene.control.*;
-import javafx.scene.layout.*;
 import javafx.scene.paint.*;
-import javafx.geometry.*;
-import javafx.scene.shape.*;
 import javafx.scene.paint.Color;
+import javafx.scene.Scene;
+import javafx.scene.shape.*;
+import javafx.scene.text.*;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
-import javafx.stage.Stage;*/
 
-public class Node
+public class Node extends Application
 {
-   public static void main(String[] args)
+   /*public static void main(String[] args)
    {
       Path path = new Path();
       path.pathGen();
       out.println(path);
-      //path.showFrame();
-   }
+   }*/
    
    public Node prev, next, up, down;
    public int data, boxX, boxY;
+   public Button btn;
    
    public Node(int n, int x, int y)
    {
@@ -47,5 +49,13 @@ public class Node
       this.data = n;
       this.boxX = x;
       this.boxY = y;
+      
+      btn = new Button();
    }
+   
+    //@Override
+    public void start(Stage primaryStage) throws Exception
+    {
+      int x = -1;
+    }
 }
